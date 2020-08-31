5dffccf88c3a8a2320a07f3639691a5cfc4badc5
vrushalisoft <57787855+vrushalisoft@users.noreply.github.com>
2020-08-31 19:56:25 +0530

    to do app done

---
+++ api/db/mongoose.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+    console.log("Connected to MongoDB successfully :)");
+}).catch((e) => {
+    console.log("Error while attempting to connect to MongoDB");
+    console.log(e);
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+
+module.exports = {
+    mongoose
+};
